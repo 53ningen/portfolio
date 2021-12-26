8fe3ccaf9bf4b4fd6eda39784953e7104c306c02
gomi_ningen <gomi2ngen@gmail.com>
2021-12-26 15:03:27 +0900

    Next.js 11.1.3

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cognito3d6258f1": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string"
+        }
+    },
+    "analytics": {
+        "portfolio": {
+            "Region": "string",
+            "Id": "string",
+            "appName": "string"
+        }
+    }
+}
